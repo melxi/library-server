f50224843e3632a8310dee4637ecb629a17bf0f7
Melxi <magomed.futijev@gmail.com>
2020-03-25 23:04:41 +0200

    initial setup

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const expressLayouts = require('express-ejs-layouts')
+const config = require('./utils/config')
+
+const app = express()
+
+const indexRouter = require('./routes/index')
+
+// Connect to MongoDB
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log('Error while connecting to MongoDB:', err.message))
+
+// Serve static assets
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+// Controllers
+app.use('/', indexRouter)
+
+
+// Connect to PORT
+app.listen(config.PORT, () => 
+  console.log(`Server running on port ${config.PORT}`)
+)
